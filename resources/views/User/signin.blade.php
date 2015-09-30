e1aa8f3ef1eb9d5a4cc3c6b8ac16bfede060bd2c
hieulespkt <hieulespkt94@gmail.com>
2015-09-30 19:45:58 +0700

    version signin

--- resources/views/User/signin.blade.php
+++ resources/views/User/signin.blade.php
@@ -9,34 +9,39 @@
     		<h3>Đăng ký</h3>
     		<p>Cảm ơn bạn đã đăng ký vào hệ thống của chúng tôi!</p>
     	</div>
-        <div clas="group-control mrbt_10">
+        <form action="{{url('user/signin')}}" method="post">
+        <div class="group-control mrbt_10">
         	<label>Họ và tên: </label>
-        	<input type='text' name="name" class="form-control" />
+        	<input type='text' name="name" class="form-control" value="$Post['name']" placeholder="Nguyễn văn A" />
         </div>
-        <div clas="group-control">
+        <div class="group-control mrbt_10">
         	<label>Tên đăng nhập: </label>
-        	<input type='text' name="username" class="form-control" />
+        	<input type='text' name="username" class="form-control" value="$Post['username']" placeholder="abc"/>
         </div>
-        <div clas="group-control">
+        <div class="group-control mrbt_10">
         	<label>Mật khẩu: </label>
-        	<input type='password' name="password" class="form-control" />
+        	<input type='password' name="password" class="form-control" placeholder="abc"/>
         </div>
-        <div clas="group-control">
+        <div class="group-control mrbt_10">
         	<label>Nhập lại mật khẩu: </label>
-        	<input type='password' name="etpassword" class="form-control" />
+        	<input type='password' name="etpassword" class="form-control" placeholder="abc"/>
         </div>
-        <div clas="group-control">
+        <div class="group-control mrbt_10">
         	<label>Email: </label>
-        	<input type='email' name="email" class="form-control" />
+        	<input type='email' name="email" class="form-control" value="$Post['email']" placeholder="abc@xyz.com"/>
         </div>
-        <div clas="group-control">
+        <div class="group-control mrbt_10">
         	<label>Địa chỉ: </label>
-        	<input type='text' name="address" class="form-control" />
+        	<input type='text' name="address" class="form-control" value="$Post['address']" placeholder="Quận 9, tp.Hồ Chí Minh"/>
         </div>
-        <div clas="group-control">
+        <div class="group-control mrbt_10">
         	<label>Số điện thoại: </label>
-        	<input type='number' name="numberphone" class="form-control" />
+        	<input type='number' name="numberphone" class="form-control" value="$Post['numberphone']" placeholder="0905123123" />
         </div>
+        <div class="group-control mrbt_10">
+            <input type="submit" name="submit" value="Đăng ký" class="btn btn-primary">
+        </div>
+       </form>
     </div>
 </div>
 @stop
